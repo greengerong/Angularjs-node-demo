bd779eab0eba57598de943819997f18f81c9e091
=greengerong <=grzx2210@163.com>
2013-04-08 22:39:01 +0800

    spike for messagebox, code should move to another file

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -9,4 +9,40 @@ app.
       otherwise({redirectTo: '/items'});
 }]).factory("underscore",["$window",function($window){
 	return $window._;
+}])
+
+app.value("messageBoxContainer","messageBoxContainer").
+value("messageBoxColor",{"error":"alert-error","warring":"","info":"alert-info"}).
+ factory("errorBoxService",["messageBoxContainer","messageBoxColor",function(messageBoxContainer,messageBoxColor){
+	var $container = $("#" +messageBoxContainer)
+ 	$container.bind("click",function(){
+		$container.find(".alert-body").toggle();
+	});
+    
+	return {
+		show : function(title,content,type){
+		$container.html('<div class="box alert alert-block ' + messageBoxColor[type] + ' fade in">' +
+	      '<button data-dismiss="alert" class="close" type="button">×</button>' +
+	      '<h4 class="alert-heading">' + title + '</h4>' + 
+	      '<div class="alert-body hide">' + content + '<div>' +
+    	'</div>');
+			$container.show();
+		},
+		hide : function(){
+		    $container.html("");
+		}
+	};
 }]);
+
+var errorBoxServiceTestController = function ($scope,errorBoxService) {
+	
+     var i = 0;
+     var type =["info","error","warring"];
+	 $scope.change =function(){
+	 	errorBoxService.show("hi should be change " + (i++),"<p>nothing<a>link</a></p>",type[i%3]);
+	 };
+	 $scope.hide =function(){
+	 	errorBoxService.hide();
+	 }
+}
+app.controller("errorBoxServiceTestController",errorBoxServiceTestController);
